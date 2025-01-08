4eb4bbe4f8362fbd6cac2f5d1dd1a7384c956b82
n-s-iroagba <nnamdisolomon1@gmail.com>
2025-01-08 14:33:57 +0100

    add how it works icons

--- client/src/components/HowItWorks.tsx
+++ client/src/components/HowItWorks.tsx
@@ -1,23 +1,29 @@
 import React from 'react';
 import { Row, Col, Card } from 'react-bootstrap';
 import '../assets/styles/HowItWorks.css'
+import { faArrowAltCircleLeft, faArrowAltCircleRight, faSearch, faUser } from '@fortawesome/free-solid-svg-icons';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 
 const HowItWorks: React.FC = () => {
   const steps = [
     {
-      title: '1. Click the button for the preferred conversation',
+      icon:faUser,
+      title: 'Click the button for the preferred conversation',
       text: 'Select the type of conversation you want to initiate by clicking the appropriate button.',
     },
     {
-      title: '2. Search celeb',
+      icon:faSearch,
+      title: 'Search celeb',
       text: 'Find your favorite celebrity by searching their name in our search bar.',
     },
     {
-      title: '3. Book a schedule (for calls only)',
+      icon:faArrowAltCircleLeft,
+      title: ' Book a schedule (for calls only)',
       text: 'Schedule a call at a time that is convenient for you.',
     },
     {
-      title: '4. Get a response',
+      icon:faArrowAltCircleRight,
+      title: ' Get a response',
       text: 'Receive a prompt response tailored to your selected conversation type.',
     },
   ];
@@ -30,6 +36,7 @@ const HowItWorks: React.FC = () => {
             <Card className="shadow-sm bg-grey rounded-4 border-0 h-100">
               <Card.Body>
                 <Card.Title className="text-start">{step.title}</Card.Title>
+                <FontAwesomeIcon color='black' icon ={step.icon}  />
                 <Card.Text className="text-start">{step.text}</Card.Text>
               </Card.Body>
             </Card>
