f61a818e14dc24e44ecabc6a8d81a2cb64d061ee
n-s-iroagba <nnamdisolomon1@gmail.com>
2025-03-29 02:46:06 +0100

    defined models

--- server/src/models/Cart.ts
+++ server/src/models/Cart.ts
@@ -1,21 +1,80 @@
-import { Optional, Model } from "sequelize";
+import {
+  Model,
+  DataTypes,
+  ForeignKey,
+  BelongsToGetAssociationMixin,
+} from "sequelize";
+import sequelize from "../config/orm";
+import { Celebrity } from "./Celebrity";
+import { Fan } from "./Fan";
+import { ClubMembership } from "./ClubMembership";
+import { Charity } from "./Charity";
+import { Ticket } from "./Ticket";
+import { Souvenir } from "./Souvenir";
+import { Tour } from "./Tour";
 
-  interface AdminAttributes {
-    id: number;
-    name: string;
-    email: string;
-    password: string;
+export interface CartAttributes {
+  id: number;
+  celebrityId: ForeignKey<Celebrity["id"]>;
+  fanId: ForeignKey<Fan["id"]>;
+  itemType: "ClubMembership" | "Charity" | "Ticket" | "Souvenir" | "Tour";
+  itemId: number;
+}
+
+export class Cart extends Model<CartAttributes> implements CartAttributes {
+  public id!: number;
+  public celebrityId!: number;
+  public fanId!: number;
+  public itemType!: "ClubMembership" | "Charity" | "Ticket" | "Souvenir" | "Tour";
+  public itemId!: number;
+
+  public readonly createdAt!: Date;
+  public readonly updatedAt!: Date;
+
+  // Association Methods
+  public getCelebrity!: BelongsToGetAssociationMixin<Celebrity>;
+  public getFan!: BelongsToGetAssociationMixin<Fan>;
+}
+
+Cart.init(
+  {
+    id: {
+      type: DataTypes.INTEGER,
+      autoIncrement: true,
+      primaryKey: true,
+    },
+    celebrityId: {
+      type: DataTypes.INTEGER,
+      references: {
+        model: Celebrity,
+        key: "id",
+      },
+      allowNull: false,
+    },
+    fanId: {
+      type: DataTypes.INTEGER,
+      references: {
+        model: Fan,
+        key: "id",
+      },
+      allowNull: false,
+    },
+    itemType: {
+      type: DataTypes.ENUM("ClubMembership", "Charity", "Ticket", "Souvenir", "Tour"),
+      allowNull: false,
+    },
+    itemId: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+    },
+  },
+  {
+    sequelize,
+    tableName: "cart",
   }
-  
-  // type AdminCreationAttributes = Optional<AdminAttributes, "id">;
-  
-  // export class Admin extends Model<AdminAttributes, AdminCreationAttributes>
-  //   implements AdminAttributes {
-  //   public id!: number;
-  //   public name!: string;
-  //   public email!: string;
-  //   public password!: string;
-  //   public readonly createdAt!: Date;
-  //   public readonly updatedAt!: Date;
-  // }
+);
+
+
+export default Cart;
+
   
